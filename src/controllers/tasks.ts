e5d51442a56520d230282a7cf02fe50fbab70eb6
Juan Jose Vega <juanvega.tech@gmail.com>
2021-05-03 20:40:31 -0500

    add controller create and read

---
+++ src/controllers/tasks.ts
@@ -0,0 +1,35 @@
+import server from "../index";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { Db } from "mongodb";
+
+export const createTask = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  try {
+    const db = server.mongo.db as Db;
+    const { title } = request.body;
+    const data = {
+      title,
+    };
+    const createdTask = await db.collection("tasksUser").insertOne(data);
+    return { id: createdTask.insertedId };
+  } catch (error) {
+    server.log.error("error inserting data");
+    server.log.error(error);
+  }
+};
+
+export const readTasks = async (
+    request: FastifyRequest,
+    reply: FastifyReply
+  ) => {
+    try {
+      const db = server.mongo.db as Db;
+      const readTasks = await db.collection('tasksUser').find().toArray();
+      return readTasks;
+    } catch (error) {
+      server.log.error('error reading data');
+      server.log.error(error);
+    }
+  };
