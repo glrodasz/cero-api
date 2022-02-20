a4f42b2d8827167f2b442d8645196f38e4b61dd3
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-02-20 10:58:31 -0500

    chore: add simple get information of api running

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import fastifyCors from 'fastify-cors'
 import swagger from 'fastify-swagger'
 import dotenv from 'dotenv'
+import os from 'os'
 
 import dbConnector from './db/connection'
 import tasksRoutes from './routes/tasks'
@@ -9,6 +10,7 @@ import options from './utils/swagger'
 
 dotenv.config()
 
+const { PORT: port = 3000 } = process.env
 const server = fastify({ logger: { prettyPrint: true } })
 
 server.register(dbConnector)
@@ -22,7 +24,11 @@ tasksRoutes.forEach((route) => {
   server.route(route)
 })
 
-server.listen(process.env.PORT || 3000, '0.0.0.0', (err, address) => {
+server.get('/', async (request, reply) => {
+  reply.send({ message: 'Reto API running', instance: os.hostname() })
+})
+
+server.listen(port, '0.0.0.0', (err) => {
   if (err) {
     console.log(err)
     process.exit(1)
